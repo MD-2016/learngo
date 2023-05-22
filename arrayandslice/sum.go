86cc65ee7216ee880ef019b07032c99ef9f6fa53
MD <25043596+MD-2016@users.noreply.github.com>
2023-05-21 21:46:36 -0400

    added tests for summing numbers with arrays and slices

---
+++ arrayandslice/sum.go
@@ -0,0 +1,34 @@
+package arrayandslice
+
+func Sum(numbers []int) int {
+	sum := 0
+
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
+
+func SumAll(numbers ...[]int) []int {
+	var sums []int
+
+	for _, nums := range numbers {
+		sums = append(sums, Sum(nums))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
